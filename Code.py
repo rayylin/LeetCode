d55bcf7221ec5dd51b98803a2a103430782a6164
rayl <dwadeflashback@gmail.com>
2025-01-27 23:11:49 -0500

    Method1

--- Code.py
+++ Code.py
@@ -1,1 +1,16 @@
-print(123)
+# two sum
+
+def twoSum1(lst:[int], tar:int):
+    for i in range(len(lst)-1):
+        if tar - lst[i] in lst[i+1:]:
+            return True 
+    return False
+
+
+if __name__ == "__main__":
+    
+    target = 12
+    l = [6,6,9]
+
+    print(twoSum1(l, target))
+
