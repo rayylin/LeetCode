fdd64dd30d01ea9bdd561b2afadb59d8a7cf6df3
rayl <dwadeflashback@gmail.com>
2025-01-27 23:18:42 -0500

    method2

--- Code.py
+++ Code.py
@@ -6,11 +6,22 @@ def twoSum1(lst:[int], tar:int):
             return True 
     return False
 
+def twoSum2(lst:[int], tar:int):
+    dic = {}
+
+    for i in lst:
+        if i not in dic.keys():
+            dic[tar-i] = 1
+        else:
+            return True
+    return False
+
 
 if __name__ == "__main__":
     
     target = 12
-    l = [6,6,9]
+    l = [5,7,9]
 
     print(twoSum1(l, target))
+    print(twoSum2(l, target))
 
